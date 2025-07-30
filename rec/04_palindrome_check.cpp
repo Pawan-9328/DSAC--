484cbd460bc9424343b9045d3816b2ea44969ac0
pawan <golapawan941@gmail.com>
2025-07-30 17:56:58 +0530

    palindrome  using only single var in rec

--- rec/04_palindrome_check.cpp
+++ rec/04_palindrome_check.cpp
@@ -2,24 +2,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool checkPalindrome(string &str, int i, int j)
+// bool checkPalindrome(string &str, int i, int j)
+// {
+//     if (i > j)
+//     {
+//         return true;
+//     }
+//     if (str[i] != str[j])
+//     {
+//         return false;
+//     }
+//     return checkPalindrome(str, i + 1, j - 1);
+// }
+
+//........ without extra var 
+
+bool checkPalindrome(string &str, int i)
 {
-    if (i > j)
+    int n = str.length(); 
+
+    if (i >= n-i-1)
     {
         return true;
     }
-    if (str[i] != str[j])
+    if (str[i] != str[n-i-1])
     {
         return false;
     }
-    return checkPalindrome(str, i + 1, j - 1);
+    return checkPalindrome(str, i + 1);
 }
 
 int main()
 {
     string str = "madam";
 
-    bool ans = checkPalindrome(str, 0, str.length()-1);
+    bool ans = checkPalindrome(str, 0);
 
     if (ans == 1)
     {

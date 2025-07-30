6eb975c7d239664cbee560085accd0a307d28245
pawan <golapawan941@gmail.com>
2025-07-30 17:51:41 +0530

    palindrome find using rec

---
+++ rec/04_palindrome_check.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkPalindrome(string &str, int i, int j)
+{
+    if (i > j)
+    {
+        return true;
+    }
+    if (str[i] != str[j])
+    {
+        return false;
+    }
+    return checkPalindrome(str, i + 1, j - 1);
+}
+
+int main()
+{
+    string str = "madam";
+
+    bool ans = checkPalindrome(str, 0, str.length()-1);
+
+    if (ans == 1)
+    {
+        cout << "Palindrome " << endl;
+    }
+    else
+    {
+        cout << "Not palindrome " << endl;
+    }
+    return 0;
+}

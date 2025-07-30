fff22d1b536a493e7f63c7c9bcfd72057e50dd31
pawan <golapawan941@gmail.com>
2025-07-30 16:26:08 +0530

    bs

--- rec/03_binarySerach.cpp
+++ rec/03_binarySerach.cpp
@@ -2,14 +2,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// void printArr(int *arr, int s, int e)
+// {
+//     for (int i = s; i <=e; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+
 bool binarySearch(int *arr, int s, int e, int k)
 {
+    // cout << endl;
+    // printArr(arr, s, e);
 
     if (s > e)
     {
         return false;
     }
     int mid = s + (e - s) / 2;
+    //cout << "value of arr mid is " << arr[mid] << endl;
 
     if (arr[mid] == k)
     {
@@ -22,14 +34,14 @@ bool binarySearch(int *arr, int s, int e, int k)
     }
     else
     {
-        return binarySearch(arr, s, e = mid - 1, k);
+        return binarySearch(arr, s,mid - 1, k);
     }
 }
 
 int main()
 {
     int arr[] = {2, 4, 6, 8, 10};
-    int key = 18;
+    int key = 8;
     bool ans = binarySearch(arr, 0, 5, key);
 
     if (ans == 1)

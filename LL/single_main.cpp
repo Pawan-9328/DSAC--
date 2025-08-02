766f572992d3135c68bdbb487b832d10b5510654
pawan <golapawan941@gmail.com>
2025-08-03 05:13:32 +0530

    deletion in singly node for every place like ,  head , middle , last

--- LL/single_main.cpp
+++ LL/single_main.cpp
@@ -15,6 +15,18 @@ public:
         this->data = data;
         this->next = NULL;
     }
+    // destructor
+    ~Node()
+    {
+        int value = this->data;
+        // memory free
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+        cout << "Memory is free with data " << value << endl;
+    }
 };
 
 void insertionHead(Node *&head, int data)
@@ -44,6 +56,77 @@ void printHead(Node *&head)
     cout << endl;
 }
 
+void insertAtMiddle(Node *&head, Node *&tail, int position, int data)
+{
+    if (position == 1)
+    {
+        insertionHead(head, data);
+        return;
+    }
+    Node *temp = head;
+    int cnt = 1;
+
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // inserting at least position
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, data);
+        return;
+    }
+
+    // creating a new node
+    Node *nodeToInsert = new Node(data);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
+
+void deleteNode(int position, Node *&head, Node *&tail)
+{
+    // deleting first node
+
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+
+        // If only one node and it's tail
+        if (temp == tail)
+        {
+            tail = NULL;
+        }
+        // free memory
+        temp->next = NULL;
+        // delete temp;
+        delete temp;
+    }
+    else
+    {
+        // deleting middle and last node
+        Node *curr = head;
+        Node *prev = NULL;
+        int cnt = 1;
+        while (cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+        prev->next = curr->next;
+// ✅ Fix here: update tail if last node is deleted
+        if (curr == tail)
+        {
+            tail = prev;
+        }
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
 int main()
 {
     Node *node1 = new Node(12);
@@ -60,6 +143,17 @@ int main()
     printHead(head);
     insertAtTail(tail, 15);
     printHead(head);
+    cout << tail->data << endl;
+    insertAtMiddle(head, tail, 2, 11);
+    printHead(head);
+    cout << head->data << endl;
+    cout << tail->data << endl;
+    cout << endl;
+    printHead(head);
+    deleteNode(4, head, tail);
+    printHead(head);
+    cout << "Head " << head->data << endl;
+    cout << "Tail " << tail->data << endl;
 
     return 0;
 }
